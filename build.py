386c7c3d2386fbe6cbd9df3b483a08d0bc8f238b
leon <leonvita91@gmail.com>
2023-01-23 22:57:59 +0000

    add demo

--- build.py
+++ build.py
@@ -1,5 +1,10 @@
 import sqlite3
 
+# The goal
+# collect student info
+# do add delete call insert
+# orginesd the code to be more flexable
+
 # build database
 build = sqlite3.connect('database.db')
 connect = build.cursor()
@@ -22,6 +27,21 @@ connect.execute(""" CREATE TABLE IF NOT EXISTS status(
     score
 )
 """)
+name = 'user'
+add = 'aaa'
+age = '234'
+gender = 'asdfafd'
+time = '32324'
+date = '234324'
+connect.execute("INSERT INTO student VALUES (?,?,?,?,?,?)",
+(
+(name),
+(add),
+(age),
+(gender),
+(time),
+(date)
+))
 connect.execute('SELECT * FROM student ')
 test = connect.fetchall()
 print(test)
