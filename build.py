66200ce9b77e2f89282a635e73ce380d1239eddc
leon <leonvita91@gmail.com>
2023-01-24 13:31:13 +0000

    add tables and checking if the database is missing

--- build.py
+++ build.py
@@ -1,47 +1,52 @@
 import sqlite3
+import os
+import colors
 
-# The goal
-# collect student info
-# do add delete call insert
-# orginesd the code to be more flexable
-
+#later should add backup functionillty
 # build database
-build = sqlite3.connect('database.db')
-connect = build.cursor()
+class Database():
+    def __init__(self):
+        #initialize the functions
+        check = self.check_db()
+        if check == True:
+            print('database initialize....')
+        elif check == False:
+            print(colors.colors().red,"""Database Not exist ??
+            \nWarning if the database not exist it will:
+            Creating a new one >>>
+            please Check the backup file.
+            """,colors.colors().end)
 
-# Create first table
-connect.execute(""" CREATE TABLE IF NOT EXISTS student(
-    name text,
-    address text,
-    age,
-    gender,
-    time,
-    date
-)
-""")
+        self.build_db()
+    def build_db(self):    
+        self.connect = sqlite3.connect('School.db')
+        self.cursor = self.connect.cursor()
+        #Create first table
+        self.cursor.execute(""" CREATE TABLE IF NOT EXISTS student(
+            name text,
+            address text,
+            age,
+            gender,
+            time,
+            date
+        )
+        """)
+        # Create second Table
+        self.cursor.execute(""" CREATE TABLE IF NOT EXISTS status(
+            materials,
+            class,
+            score
+        )
+        """)
+    def check_db(self):
+        self.path = []
+        self.read = os.listdir('.')
+        for i in self.read:
+            if i == 'School.db':
+                self.path.append(i)
+        if self.path == ['School.db']:
+            return True
+        if self.path != ['School.db']:
+            return False
 
-# Create second Table
-connect.execute(""" CREATE TABLE IF NOT EXISTS status(
-    materials,
-    class,
-    score
-)
-""")
-name = 'user'
-add = 'aaa'
-age = '234'
-gender = 'asdfafd'
-time = '32324'
-date = '234324'
-connect.execute("INSERT INTO student VALUES (?,?,?,?,?,?)",
-(
-(name),
-(add),
-(age),
-(gender),
-(time),
-(date)
-))
-connect.execute('SELECT * FROM student ')
-test = connect.fetchall()
-print(test)
+Database()
---
+++ colors.py
@@ -0,0 +1,13 @@
+
+
+# add color to texts.
+class colors:
+    pink = '\033[95m'
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    red = '\033[91m'
+    end = '\033[0m'
+    bold = '\033[1m'
+    uderline = '\033[4m'
