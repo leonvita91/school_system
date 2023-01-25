9a894b4bd4e795c179fa351dc76ee2528514b598
leon <leonvita91@gmail.com>
2023-01-25 12:09:18 +0000

    finish backup plan

--- backup_plan.py
+++ backup_plan.py
@@ -3,20 +3,23 @@ import shutil
 from datetime import datetime
 
 now = datetime.now()
-dates = now.strftime("%Y-%m-%d")
+dates = now.strftime("%d-%m-%Y")
 times = now.strftime("%H:%M:%S")
+
+# Backup class & methods
 class Backup():
     def __init__(self):
         # initialize backup plan
         self.snap()
     def snap(self):
+        self.current_time = f"{dates} " + f"{times}" 
         self.show = build.Database
         self.result = self.show.check_db(self)
         if self.result == True:
             print('backing up database.....')
-            shutil.copy('School.db','backup')
+            shutil.copyfile('School.db', f'{self.current_time}')
+            shutil.move(f'{self.current_time}','backup')
         if self.result == False:
             print('Sorry datebase not Exist !!')
             print("can't take backup now... please try to restored you backup ")
 
-Backup()
--- input.py
+++ input.py
@@ -1,9 +1,3 @@
 import build
 from datetime import datetime
 from time import sleep
-
-now = datetime.now()
-dates = now.strftime("%Y-%m-%d")
-times = now.strftime("%H:%M:%S")
-
-print(dates,times)
