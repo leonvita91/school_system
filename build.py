68ff29487e4cbc053714d3b1aeab6d7e98d129f3
leon <leonvita91@gmail.com>
2023-01-27 16:11:48 +0000

    trying to fix if statment

--- build.py
+++ build.py
@@ -9,6 +9,7 @@ class Database():
         #initialize the functions
         self.check_exist_db()
         self.build_db()
+        self.add_sub()
     def build_db(self):    
         self.connect = sqlite3.connect('School.db')
         self.cursor = self.connect.cursor()
@@ -31,11 +32,11 @@ class Database():
         )
         """)
         #Create 3th Table
-        self.cursor.execute(f""" CREATE TABLE IF NOT EXISTS mat(
+        self.cursor.execute(f"""CREATE TABLE IF NOT EXISTS mat(
             subject_id integer PRIMARY KEY AUTOINCREMENT,
             subjects text
-                )
-                """)
+        )
+        """)
     
     #searching on Database if exist
     def check_exist_db(self):
@@ -59,6 +60,26 @@ class Database():
             \n----------------------------------------------
             """,colors.colors().end)
             return False
+        # Insert Subjects Into db
+    
+    def add_sub(self):
+        print('Add Subject ?')
+        user_dec = str(input('yes/no:\n'))
+        if user_dec == 'yes' or 'y':
+            self.sub_in = str(input('Subject Name: '))
+            self.cursor.execute("INSERT INTO mat(subjects) VALUES (?)",
+            (
+            (self.sub_in)
+            ))
+            print("Would you like to add another Subject ? ")
+            # ask user again
+            self.try_again = str(input('yes/no: '))
+            if self.try_again == 'y' or 'yes':
+                self.add_sub()
+            elif self.try_again == 'n' or 'no':
+                pass
+        elif user_dec == 'no' or 'n':
+            pass
 
 if __name__ == '__main__':
     Database()
